2f35d109183a868cb0b717ce60bf0c1377a34590
Ricky Liu <rickyhl02@berkeley.edu>
2024-02-20 20:00:44 -0800

    first pass at items

---
+++ Assets/Scripts/ItemInstance.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemInstance : MonoBehaviour
+{
+    public ItemInfo m_itemInfo;
+
+    private Text m_name;
+
+    private Image m_image;
+    
+    void Start()
+    {
+        m_name = GetComponent<Text>();
+        m_image = GetComponent<Image>();
+        m_name.text = m_itemInfo.Name;
+        m_image.sprite = m_itemInfo.Sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
