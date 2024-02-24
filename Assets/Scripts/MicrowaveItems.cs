aa44412f72f3c06319ca6b9ecd4eb9fe2c444d67
Ricky Liu <rickyhl02@berkeley.edu>
2024-02-24 00:00:47 -0800

    dragging and dropping :

--- Assets/Scripts/MicrowaveItems.cs
+++ Assets/Scripts/MicrowaveItems.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
 
 public class MicrowaveItems : MonoBehaviour
 {
@@ -18,21 +19,45 @@ public class MicrowaveItems : MonoBehaviour
     {
         combiner = gameObject.GetComponent<Combiner>();
     }
+
+ 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonUp(0))
         {
             if (HeldItem)
             {
+                Slot closestSlot = null;
+                float minDistance = 1;
+                float currDist = float.MaxValue;
+                foreach (Slot slot in slots)
+                {
+                   
+                    float dist = Vector2.Distance(Camera.main.ScreenToWorldPoint(Input.mousePosition), slot.transform.position);
+                    Debug.DrawLine(Input.mousePosition, slot.transform.position);
+                    Debug.Log(dist);
+
+                    if (dist < minDistance && dist < currDist && slot.itemInfo == null)
+                    {
+                        closestSlot = slot;
+                        currDist = dist;
+                    }
+                }
+                if (closestSlot != null && closestSlot.itemInfo == null )
+                {
+                    closestSlot.ReciveData(HeldItem.GetComponent<ItemInstance>().ItemInfo);
+                    //closestSlot.itemInstance = Instantiate(HeldItem, closestSlot.transform.position, Quaternion.identity)
+                    closestSlot.spriteRenderer.sprite = closestSlot.itemInfo.Sprite;
+                }
                 if (slots[0].itemInfo == null)
                 {
-                    slots[0].ReciveData(HeldItem.GetComponent<ItemInstance>().ItemInfo);
-                    slots[0].spriteRenderer.sprite = slots[0].itemInfo.Sprite;
+                    //slots[0].ReciveData(HeldItem.GetComponent<ItemInstance>().ItemInfo);
+                    //slots[0].spriteRenderer.sprite = slots[0].itemInfo.Sprite;
                 }
                 else if (slots[1].itemInfo == null)
                 {
-                    slots[1].ReciveData(HeldItem.GetComponent<ItemInstance>().ItemInfo);
-                    slots[1].spriteRenderer.sprite = slots[1].itemInfo.Sprite;
+                    //slots[1].ReciveData(HeldItem.GetComponent<ItemInstance>().ItemInfo);
+                    //slots[1].spriteRenderer.sprite = slots[1].itemInfo.Sprite;
                 }
             }
             HeldItem = null;
@@ -51,6 +76,7 @@ public class MicrowaveItems : MonoBehaviour
     {
         foreach(Slot slot in slots)
         {
+            //Destroy(slot.gameObject);
             slot.itemInfo = null;
             slot.spriteRenderer.sprite = null;
         }
--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Slot : MonoBehaviour
 {
+    public GameObject itemInstance;
     public ItemInfo itemInfo;
     public int slotIndex;
     public SpriteRenderer spriteRenderer;
@@ -16,6 +17,11 @@ public class Slot : MonoBehaviour
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Instantiate(collision.gameObject, this.transform.position, Quaternion.identity);
     }
 }
 
